a0ce054919179654c5ee6f018319d2c915684fda
olesya <nearolesya@gmil.com>
2023-10-13 17:35:22 +0300

    kod final task

---
+++ task/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+string[] array = new string[4] {"Hello", "2", "world", ":-)"};
+string[] finalarray = new string[array.Length];
+
+void FinalArray (string[] array, string[] finalarray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        finalarray[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int index = 0; index < array.Length; index ++)
+    {
+        Console.Write(array[index] + " ");
+    }
+}
+FinalArray(array, finalarray);
+PrintArray(finalarray);
